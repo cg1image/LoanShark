1678a1894eba9e8ee56babda55ed4dc10de956cc
CGovers <54491781+cg1image@users.noreply.github.com>
2022-08-05 22:39:14 -0700

    update Loan Shark
    mortgage calc function in progress

--- js/site.js
+++ js/site.js
@@ -33,6 +33,12 @@ function mortgageCalc(loanAmount, term, rate){
 
     returnArray = [];
          
+    for (let index = 0; index <= 5; index++) {
+        
+        if (condition) {
+            returnArray.length
+        }
+    }
 }
 
 
